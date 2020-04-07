f8ac31a69dc6c9eb6b00d88c030d23229b7d14f7
admin <1762438122@qq.com>
2020-04-07 12:06:49 +0800

    new 2020/4/7

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180111/Ch6Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180111/Ch6Activity1.java
@@ -2,12 +2,21 @@ package cn.edu.sdwu.android.classroom.sn170507180111;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class Ch6Activity1 extends AppCompatActivity {
+public class demo01_4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ch61);
+        setContentView(R.layout.demo01_style4);
+        //在JAVA中获取字符串资源
+        String content=getString(R.string.hello);
+        Log.i(demo01_4.this.toString(),content);
+
+
+        String sms=getString(R.string.sms);
+        String.format(sms,100,"张三");
+        Log.i(demo01_4.this.toString(),sms);
     }
 }
